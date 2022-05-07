5cb89da60e57e91224ab2eacbe333b968b055e73
Yasmine <yasmine.souissi@esprit.tn>
2022-05-07 18:44:05 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ContactList from "./Components/contactList";
+import AddContact from "./Components/addContact";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ContactList />
+            <h1>Add contact</h1>
+            <AddContact />
+        </div>
+    );
 }
 
 export default App;
